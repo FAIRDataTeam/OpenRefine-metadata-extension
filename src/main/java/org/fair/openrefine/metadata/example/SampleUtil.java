42a4304359b911b55131aa92f59ab2097d7d7f8d
Marek Suchánek <marek.suchanek@fit.cvut.cz>
2019-09-05 18:01:11 +0200

    Improve project structure according Maven guidelines

---
+++ src/main/java/org/fair/openrefine/metadata/example/SampleUtil.java
@@ -0,0 +1,9 @@
+package org.fair.openrefine.metadata.example;
+
+public class SampleUtil {
+    // Just an example file
+
+    static public int sampleMethod() {
+        return 42;
+    }
+}
---
+++ src/test/java/org/fair/openrefine/metadata/example/SampleUtilTest.java
@@ -0,0 +1,14 @@
+package org.fair.openrefine.metadata.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleUtilTest {
+    // Just an example test file
+
+    @Test
+    public void testSampleMethod() {
+        assertEquals(42, SampleUtil.sampleMethod());
+    }
+}
