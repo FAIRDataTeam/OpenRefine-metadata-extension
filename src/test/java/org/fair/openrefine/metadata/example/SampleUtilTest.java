386c1fc379d8b9c38846f02eefd661033dd380e0
Marek Suchánek <suchama4@fit.cvut.cz>
2019-09-24 11:23:02 +0200

    Update Maven dependencies

--- src/test/java/org/fair/openrefine/metadata/example/SampleUtilTest.java
+++ src/test/java/org/fair/openrefine/metadata/example/SampleUtilTest.java
@@ -22,9 +22,10 @@
  */
 package org.fair.openrefine.metadata.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class SampleUtilTest {
     // Just an example test file
