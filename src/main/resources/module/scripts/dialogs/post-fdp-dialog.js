d5bbf56e171ecd9c82da87eb52d83b36f757a554
Marek Suchánek <MarekSuchanek@users.noreply.github.com>
2019-09-27 18:37:13 +0200

    [FOR-13] Display FDP metadata or error (#3)

---
+++ src/main/resources/module/scripts/dialogs/post-fdp-dialog.js
@@ -0,0 +1,85 @@
+/* global $, DOM, DialogSystem, Refine */
+var PostFdpDialog = {};
+
+PostFdpDialog.launch = function() {
+    this.frame = $(DOM.loadHTML("metadata", "scripts/dialogs/post-fdp-dialog.html"));
+    this._elmts = DOM.bind(this.frame);
+
+    this._level = DialogSystem.showDialog(this.frame);
+
+    var self = this;
+    var elmts = this._elmts;
+
+    elmts.dialogTitle.text($.i18n("post-fdp-dialog/title"));
+    elmts.closeButton.text($.i18n("post-fdp-dialog/button-close"));
+    elmts.baseURI.attr("title", $.i18n("post-fdp-dialog/description"));
+    elmts.connectButton.text($.i18n("post-fdp-dialog/button-connect"));
+    elmts.baseURILabel.text($.i18n("post-fdp-dialog/label-uri"));
+
+    elmts.baseURI.focus();
+    elmts.baseURI[0].setSelectionRange(0, elmts.baseURI.val().length);
+
+    const dismiss = () => {
+        DialogSystem.dismissUntil(self._level - 1);
+    };
+
+    const fdpMakeURL = (uriObject) => {
+        return uriObject.namespace + uriObject.localName;
+    };
+
+    const showFDPMetadata = (fdpMetadata) => {
+        let title = $("<a>")
+            .attr("href", fdpMakeURL(fdpMetadata.uri))
+            .attr("target", "_blank")
+            .text(fdpMetadata.title.label)
+            .get(0).outerHTML;
+        let publisher = $("<a>")
+            .attr("href", fdpMakeURL(fdpMetadata.publisher.uri))
+            .attr("target", "_blank")
+            .text(fdpMetadata.publisher.name.label)
+            .get(0).outerHTML;
+        let description = fdpMetadata.description.label;
+
+        let table = $("<table>")
+            .append("<tr><th>Title</th><td>" + title + "</td></tr>")
+            .append("<tr><th>Publisher</th><td>" + publisher + "</td></tr>")
+            .append("<tr><th>Description</th><td>" + description + "</td></tr>");
+
+        elmts.fdpMetadata
+            .append("<p>" + $.i18n("post-fdp-dialog/connected-to-fdp") + "<p>")
+            .append(table);
+    };
+
+    elmts.closeButton.click(function() {
+        dismiss();
+    });
+
+    elmts.connectButton.click(function() {
+        var fdpURI = elmts.baseURI.val();
+        elmts.warningsArea.text("");
+        elmts.fdpConnected.addClass("hidden");
+        elmts.fdpConnectionError.addClass("hidden");
+
+        Refine.postProcess(
+            "metadata",
+            "connect-fdp",
+            {},
+            { uri: fdpURI },
+            {},
+            {
+                onDone(o) {
+                    if (o.status === "ok") {
+                        elmts.fdpConnected.removeClass("hidden");
+                        showFDPMetadata(o.fdpMetadata);
+                    } else {
+                        elmts.fdpConnectionError.removeClass("hidden");
+                        elmts.warningsArea.text($.i18n(o.message));
+                    }
+                },
+                onError() {
+                    elmts.warningsArea.text($.i18n("connect-fdp-command/error"));
+                }
+            }
+        );
+    });
+};
