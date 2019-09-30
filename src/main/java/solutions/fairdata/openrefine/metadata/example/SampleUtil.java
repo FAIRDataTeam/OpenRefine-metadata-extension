dfd294b18f64b2ccfcd451bb21e2e9030729b51c
Marek Suchánek <MarekSuchanek@users.noreply.github.com>
2019-09-30 13:06:09 +0200

    Refactor package prefix to solutions.fairdata (#5)

--- src/main/java/solutions/fairdata/openrefine/metadata/example/SampleUtil.java
+++ src/main/java/solutions/fairdata/openrefine/metadata/example/SampleUtil.java
@@ -20,18 +20,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.fair.openrefine.metadata.example;
+package solutions.fairdata.openrefine.metadata.example;
 
-import org.junit.jupiter.api.Test;
+public class SampleUtil {
+    // Just an example file
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-public class SampleUtilTest {
-    // Just an example test file
-
-    @Test
-    public void testSampleMethod() {
-        assertEquals(42, SampleUtil.sampleMethod());
+    static public int sampleMethod() {
+        return 42;
     }
 }
--- src/main/java/solutions/fairdata/openrefine/metadata/fdp/FairDataPointException.java
+++ src/main/java/solutions/fairdata/openrefine/metadata/fdp/FairDataPointException.java
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.fair.openrefine.metadata.fdp;
+package solutions.fairdata.openrefine.metadata.fdp;
 
 public class FairDataPointException extends Exception {
 
