5a4ddb176fbbef9a8ae215c46f2f5d22212f15bc
Marek Suchánek <marek.suchanek@gmail.com>
2019-10-08 19:46:24 +0200

    Add dataset creation dialog

--- src/main/resources/module/scripts/metadata-specs.js
+++ src/main/resources/module/scripts/metadata-specs.js
@@ -60,5 +60,77 @@ MetadataSpecs.catalog = {
             "required": true,
         },
     ]
-} ;
+};
 
+MetadataSpecs.dataset = {
+    "id": "dataset",
+    "fields": [
+        {
+            "id": "title",
+            "type": "string",
+            "required": true,
+        },
+        {
+            "id": "version",
+            "type": "string",
+            "required": true,
+        },
+        {
+            "id": "publisher",
+            "type": "iri",
+            "required": true,
+            "nested": {
+                "fields": [
+                    {
+                        "id": "publisher-name",
+                        "type": "string",
+                        "required": true,
+                    },
+                ]
+            }
+        },
+        {
+            "id": "description",
+            "type": "text",
+            "required": false,
+        },
+        {
+            "id": "language",
+            "type": "iri",
+            "required": false,
+            "multiple": true,
+        },
+        {
+            "id": "license",
+            "type": "iri",
+            "required": false,
+        },
+        {
+            "id": "rights",
+            "type": "iri",
+            "required": false,
+        },
+        {
+            "id": "theme",
+            "type": "iri",
+            "required": true,
+            "multiple": true,
+        },
+        {
+            "id": "contactPoint",
+            "type": "url",
+            "required": false,
+        },
+        {
+            "id": "keyword",
+            "type": "url",
+            "required": false,
+            "multiple": true,
+        },
+        {
+            "id": "landingPage",
+            "type": "url",
+            "required": false,
+        },
+    ]
+};
