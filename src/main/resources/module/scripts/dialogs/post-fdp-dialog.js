3c18cc629acfaf1f07d600169a5a1097c05321ab
Marek Suchánek <MarekSuchanek@users.noreply.github.com>
2019-10-10 07:36:23 +0200

    [FOR-18] Distribution creation form (#10)

--- src/main/resources/module/scripts/dialogs/post-fdp-dialog.js
+++ src/main/resources/module/scripts/dialogs/post-fdp-dialog.js
@@ -1,102 +1,269 @@
-/* global $, DOM, DialogSystem, Refine, MetadataHelpers */
-let MetadataFormDialog = {};
+/* global $, DOM, DialogSystem, MetadataHelpers, MetadataFormDialog, MetadataSpecs */
+var PostFdpDialog = {};
 
-MetadataFormDialog.launch = function(type, specs, callback) {
-    this.frame = $(DOM.loadHTML("metadata", "scripts/dialogs/metadata-form-dialog.html"));
+PostFdpDialog.launch = function() {
+    this.frame = $(DOM.loadHTML("metadata", "scripts/dialogs/post-fdp-dialog.html"));
     this._elmts = DOM.bind(this.frame);
+    this.metadata = {
+        "catalogs": [],
+        "datasets": [],
+        "distributions": []
+    };
+    this.customMetadata = {
+        "catalogs": [],
+        "datasets": [],
+        "distributions": {}
+    };
 
     this._level = DialogSystem.showDialog(this.frame);
 
     let dialog = this;
     let elmts = this._elmts;
 
-    MetadataFormDialog.initBasicTexts(dialog, type);
-    MetadataFormDialog.createForm(dialog, specs, callback);
+    PostFdpDialog.initBasicTexts(dialog);
+    PostFdpDialog.resetDefault(dialog);
+    PostFdpDialog.resetCatalogLayer(dialog);
+    PostFdpDialog.resetDatasetLayer(dialog);
+    PostFdpDialog.resetDistributionLayer(dialog);
+
+    // Focus value of FDP URI input for easy overwrite
+    elmts.baseURI.focus();
+    elmts.baseURI[0].setSelectionRange(0, elmts.baseURI.val().length);
 
     // Bind actions
-    elmts.closeButton.click(MetadataFormDialog.dismissFunc(dialog));
-    elmts.optionalShowButton.click(() => {
-        dialog.frame.find(".optional").each(function(){
-            $(this).removeClass("hidden");
-        });
-        elmts.optionalShowButton.addClass("hidden");
-        elmts.optionalHideButton.removeClass("hidden");
+    elmts.closeButton.click(PostFdpDialog.dismissFunc(dialog));
+
+    elmts.connectButton.click(() => {
+        const fdpUri = dialog._elmts.baseURI.val();
+
+        PostFdpDialog.resetDefault(dialog);
+        PostFdpDialog.ajaxFDPMetadata(dialog, fdpUri);
     });
-    elmts.optionalHideButton.click(() => {
-        dialog.frame.find(".optional").each(function(){
-            $(this).addClass("hidden");
+
+    elmts.catalogSelect.on("change", () => {
+        const catalogUri = elmts.catalogSelect.val();
+
+        PostFdpDialog.resetDatasetLayer(dialog);
+        PostFdpDialog.resetDistributionLayer(dialog);
+
+        if(catalogUri.startsWith("custom")) {
+            PostFdpDialog.showDatasets(dialog);
+        } else {
+            PostFdpDialog.ajaxDatasets(dialog, catalogUri);
+        }
+    });
+
+    elmts.datasetSelect.on("change", () => {
+        PostFdpDialog.resetDistributionLayer(dialog);
+
+        PostFdpDialog.showDistributions(dialog);
+    });
+
+    elmts.catalogAddButton.click(() => {
+        MetadataFormDialog.launch("catalog", MetadataSpecs.catalog, (newCatalog) => {
+            const index = dialog.customMetadata.catalogs.length;
+            newCatalog.id = `customCatalog-${index}`;
+            dialog.customMetadata.catalogs.push(newCatalog);
+
+            PostFdpDialog.showCatalogs(dialog);
+            dialog.frame.find(`#${newCatalog.id}`).prop("selected", true);
+            elmts.catalogSelect.trigger("change");
         });
-        elmts.optionalShowButton.removeClass("hidden");
-        elmts.optionalHideButton.addClass("hidden");
+    });
+
+    elmts.datasetAddButton.click(() => {
+        MetadataFormDialog.launch("dataset", MetadataSpecs.dataset, (newDataset) => {
+            const index = dialog.customMetadata.datasets.length;
+            newDataset.id = `customDataset-${index}`;
+            newDataset.refCatalog = elmts.catalogSelect.val();
+            dialog.customMetadata.datasets.push(newDataset);
+
+            PostFdpDialog.showDatasets(dialog);
+            dialog.frame.find(`#${newDataset.id}`).prop("selected", true);
+            elmts.datasetSelect.trigger("change");
+        });
+    });
+
+    elmts.distributionAddButton.click(() => {
+        MetadataFormDialog.launch("distribution", MetadataSpecs.distribution, (newDistribution) => {
+            newDistribution.refDataset = elmts.datasetSelect.val();
+            dialog.customMetadata.distributions[elmts.datasetSelect.val()] = newDistribution;
+
+            PostFdpDialog.showDistributions(dialog);
+        });
     });
 };
 
-MetadataFormDialog.initBasicTexts = (dialog, type) => {
+PostFdpDialog.initBasicTexts = (dialog) => {
     dialog.frame.i18n();
-    dialog._elmts.dialogTitle.text($.i18n(`metadata/${type}/dialog-title`));
+    dialog._elmts.baseURI.attr("title", $.i18n("post-fdp-dialog/description"));
 };
 
-MetadataFormDialog.dismissFunc = (dialog) => {
+PostFdpDialog.dismissFunc = (dialog) => {
     return () => { DialogSystem.dismissUntil(dialog._level - 1); };
 };
 
-MetadataFormDialog.createForm = (dialog, specs, callback) => {
+PostFdpDialog.resetDefault = (dialog) => {
+    dialog._elmts.dialogBody.find(".default-clear").empty();
+    dialog._elmts.dialogBody.find(".default-hidden").addClass("hidden");
+};
 
-    const makeLabel = (field) => {
-        return $("<label>")
-            .attr("for", field.id)
-            .text($.i18n(`metadata/${specs.id}/${field.id}/name`));
-    };
+PostFdpDialog.createSelectOption = (name) => {
+    return $("<option>")
+        .prop("disabled", true)
+        .prop("selected", true)
+        .text(`-- select a ${name} --`);
+};
 
-    const makeInput = (field) => {
-        return $(field.type === "text" ? "<textarea>" : "<input>")
-            .attr("id", field.id)
-            .attr("name", field.id)
-            .attr("type", field.type === "iri" || field.type === "url" ? "url" : "text")
-            .prop("required", field.required);
-        // TODO: multiple? (need more info)
-    };
+PostFdpDialog.resetSelect = (select, name) => {
+    select.empty();
+    select.append(PostFdpDialog.createSelectOption(name));
+};
 
-    const makeFormGroup = (field) => {
-        let fieldDiv = $("<div>")
-            .attr("id", `form-group-${field.id}`)
-            .addClass("metadata-form-group")
-            .addClass(field.required ? "required" : "optional");
+PostFdpDialog.resetCatalogLayer = (dialog) => {
+    dialog.metadata.catalogs = [];
+    PostFdpDialog.resetSelect(dialog._elmts.catalogSelect, "catalog");
+    dialog._elmts.catalogLayer.addClass("hidden");
+};
 
-        if (!field.required) { fieldDiv.addClass("hidden"); }
+PostFdpDialog.resetDatasetLayer = (dialog) => {
+    dialog.metadata.datasets = [];
+    PostFdpDialog.resetSelect(dialog._elmts.datasetSelect, "dataset");
+    dialog._elmts.datasetLayer.addClass("hidden");
+};
 
-        fieldDiv.append(makeLabel(field));
-        fieldDiv.append(makeInput(field));
+PostFdpDialog.resetDistributionLayer = (dialog) => {
+    dialog.metadata.distributions = [];
+    dialog._elmts.distributionsList.empty();
+    dialog._elmts.distributionLayer.addClass("hidden");
+};
 
-        if ("nested" in field) {
-            let nestedDiv = $("<div>")
-                .addClass("nested-fields");
-            field.nested.fields.forEach((innerField) => {
-                nestedDiv.append(makeFormGroup(innerField));
-            });
-            fieldDiv.append(nestedDiv);
+PostFdpDialog.ajaxGeneric = (dialog, command, method, data, callback) => {
+    MetadataHelpers.ajax(command, method, data,
+        (result) => {
+            if (result.status === "ok") {
+                callback(result);
+            } else {
+                dialog._elmts.fdpConnected.addClass("hidden");
+                dialog._elmts.fdpConnectionError.removeClass("hidden");
+                dialog._elmts.warningsArea.text($.i18n(result.message));
+            }
+        },
+        () => {
+            dialog._elmts.fdpConnected.addClass("hidden");
+            dialog._elmts.fdpConnectionError.removeClass("hidden");
+            dialog._elmts.warningsArea.text($.i18n("connect-fdp-command/error"));
         }
+    );
+};
 
-        return fieldDiv;
-    };
+PostFdpDialog.ajaxFDPMetadata = (dialog, fdpUri) => {
+    PostFdpDialog.ajaxGeneric(dialog, "fdp-metadata", "GET", { fdpUri },
+        (result) => {
+            dialog._elmts.fdpConnected.removeClass("hidden");
+            PostFdpDialog.showFDPMetadata(dialog, result.fdpMetadata);
 
-    specs.fields.forEach((field) => {
-        dialog._elmts.metadataForm.append(makeFormGroup(field));
+            PostFdpDialog.resetCatalogLayer(dialog);
+            PostFdpDialog.resetDatasetLayer(dialog);
+            PostFdpDialog.resetDistributionLayer(dialog);
+            PostFdpDialog.ajaxCatalogs(dialog, fdpUri);
+        }
+    );
+};
+
+PostFdpDialog.ajaxCatalogs = (dialog, fdpUri) => {
+    PostFdpDialog.ajaxGeneric(dialog, "catalogs-metadata", "GET", { fdpUri },
+        (result) => {
+            dialog._elmts.catalogLayer.removeClass("hidden");
+            dialog.metadata.catalogs = result.catalogsMetadata;
+            PostFdpDialog.showCatalogs(dialog);
+        }
+    );
+};
+
+PostFdpDialog.ajaxDatasets = (dialog, catalogUri) => {
+    PostFdpDialog.ajaxGeneric(dialog, "datasets-metadata", "GET", { catalogUri },
+        (result) => {
+            dialog._elmts.fdpConnected.removeClass("hidden");
+            dialog.metadata.datasets = result.datasetsMetadata;
+            PostFdpDialog.showDatasets(dialog);
+        }
+    );
+};
+
+PostFdpDialog.showFDPMetadata = (dialog, fdpMetadata) => {
+    let title = $("<a>")
+        .attr("href", MetadataHelpers.fdpMakeURL(fdpMetadata.uri))
+        .attr("target", "_blank")
+        .text(fdpMetadata.title.label)
+        .get(0).outerHTML;
+    let publisher = $("<a>")
+        .attr("href", MetadataHelpers.fdpMakeURL(fdpMetadata.publisher.uri))
+        .attr("target", "_blank")
+        .text(fdpMetadata.publisher.name.label)
+        .get(0).outerHTML;
+
+    dialog._elmts.fdpMetadata.append($("<p>")
+        .append($.i18n("post-fdp-dialog/connected-to-fdp"))
+        .append(" \"" + title + "\" ")
+        .append($.i18n("post-fdp-dialog/published-by"))
+        .append(" " + publisher + ".")
+    );
+};
+
+PostFdpDialog.showMetadataSelect = (select, metadatas, customMetadatas) => {
+    metadatas.forEach((metadata) => {
+        select.append(
+            $("<option>")
+                .addClass("from-fdp")
+                .attr("value", MetadataHelpers.fdpMakeURL(metadata.uri))
+                .text(metadata.title.label)
+        );
     });
-
-    const submitBtn = $("<button>")
-        .attr("type", "submit")
-        .addClass("button")
-        .text($.i18n(`metadata/${specs.id}/submit`));
-    dialog._elmts.metadataForm.append(submitBtn);
-
-    dialog._elmts.metadataForm.submit((e) => {
-        e.preventDefault();
-        let result = {};
-        specs.fields.forEach((field) => {
-            result[field.id] = dialog._elmts.metadataForm.find(`#${field.id}`).val();
-        });
-        callback(result);
-        MetadataFormDialog.dismissFunc(dialog)();
+    customMetadatas.forEach((metadata) => {
+        select.append(
+            $("<option>")
+                .addClass("custom")
+                .attr("id", metadata.id)
+                .attr("value", metadata.id)
+                .text(metadata.title)
+                .append($("<span>").text(" " + $.i18n("metadata/custom-flag")))
+        );
     });
+};
+
+PostFdpDialog.showCatalogs = (dialog) => {
+    PostFdpDialog.resetSelect(dialog._elmts.catalogSelect, "catalog");
+    PostFdpDialog.showMetadataSelect(
+        dialog._elmts.catalogSelect,
+        dialog.metadata.catalogs,
+        dialog.customMetadata.catalogs
+    );
+    dialog._elmts.catalogLayer.removeClass("hidden");
+};
+
+PostFdpDialog.showDatasets = (dialog) => {
+    PostFdpDialog.resetSelect(dialog._elmts.datasetSelect, "dataset");
+    const actCatalog = dialog._elmts.catalogSelect.val();
+    PostFdpDialog.showMetadataSelect(
+        dialog._elmts.datasetSelect,
+        dialog.metadata.datasets,
+        dialog.customMetadata.datasets.filter((d) => d.refCatalog === actCatalog)
+    );
+    dialog._elmts.datasetLayer.removeClass("hidden");
+};
+
+PostFdpDialog.showDistributions = (dialog) => {
+    dialog._elmts.distributionsList.empty();
+    const actDataset = dialog._elmts.datasetSelect.val();
+    const distribution = dialog.customMetadata.distributions[`${actDataset}`];
+    if (distribution) {
+        const item = $("<li>")
+            .addClass("custom")
+            .addClass("distribution-item")
+            .attr("id", distribution.id)
+            .text(`${distribution.title} (${distribution.version})`);
+        dialog._elmts.distributionsList.append(item);
+    }
+    dialog._elmts.distributionLayer.removeClass("hidden");
 };
--- src/main/resources/module/scripts/metadata-specs.js
+++ src/main/resources/module/scripts/metadata-specs.js
@@ -118,18 +118,59 @@ MetadataSpecs.dataset = {
         },
         {
             "id": "contactPoint",
-            "type": "url",
+            "type": "iri",
             "required": false,
         },
         {
             "id": "keyword",
-            "type": "url",
+            "type": "iri",
             "required": false,
             "multiple": true,
         },
         {
             "id": "landingPage",
-            "type": "url",
+            "type": "iri",
+            "required": false,
+        },
+    ]
+};
+
+MetadataSpecs.distribution = {
+    "id": "distribution",
+    "fields": [
+        {
+            "id": "title",
+            "type": "string",
+            "required": true,
+        },
+        {
+            "id": "license",
+            "type": "iri",
+            "required": true,
+        },
+        {
+            "id": "version",
+            "type": "string",
+            "required": true,
+        },
+        {
+            "id": "rights",
+            "type": "iri",
+            "required": false,
+        },
+        {
+            "id": "description",
+            "type": "text",
+            "required": false,
+        },
+        {
+            "id": "format",
+            "type": "string",
+            "required": false
+        },
+        {
+            "id": "bytesize",
+            "type": "string",
             "required": false,
         },
     ]
